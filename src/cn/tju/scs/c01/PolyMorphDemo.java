e41553d243a8c0c2cf47f0175acc077c107209c6
mfwzan <wangzan@tju.edu.cn>
2019-02-25 20:53:58 +0800

    Commit before class

---
+++ src/cn/tju/scs/c01/PolyMorphDemo.java
@@ -0,0 +1,79 @@
+package cn.tju.scs.c01;
+
+class PA{
+	public void print() {
+		System.out.println("Print() A");
+	}
+	
+	public void test(){
+		System.out.println("Test A");
+	}
+}
+
+class PB extends PA{
+	public void print() {
+		System.out.println("Print() B");		
+	}
+	
+	public void funB(){
+		System.out.println("NEW function of B");
+	}
+}
+
+class PC extends PA{
+	public void print(){
+		System.out.println("Print() C");
+	}
+}
+
+public class PolyMorphDemo {
+	public static void main(String[] args) {
+//		PA a = new PB();
+//		a.print();
+//		a.test();
+		
+//		PA a = new PB();
+//		PB b = (PB)a;
+//		b.print();
+//		b.test();
+		
+		/**
+		 * 向上转型：
+		 * 参数统一，可以调用子类覆写后的方法体。同一个方法针对不同的子类实现
+		 */
+//		PA a1 = new PB();
+//		PA a2 = new PC();
+//		
+//		a1.print();
+//		a2.print();
+		
+		/**
+		 * 向下转型：
+		 * 看不见子类的特殊方法，需要向下转型
+		 */
+//		PA a = new PB();
+//		a.funB();
+		
+//		PA a = new PB();
+//		PB b = (PB)a;
+//		b.funB();
+		
+//		PA a = new PB();
+//		fun(a);
+		
+		PA a = new PB();
+		System.out.println(a instanceof PA);
+		System.out.println(a instanceof PB);
+		if (a instanceof PB){
+			PB b = (PB)a;
+			b.print();
+		}
+				
+	}
+	
+	public static void fun(PA a){
+		PB b = (PB)a;
+		b.funB();
+	}
+
+}
---
+++ src/cn/tju/scs/c01/TestMainEn.java
@@ -0,0 +1,9 @@
+package cn.tju.scs.c01;
+public class TestMainEn {
+	public static void main(String[] args) {
+		CarEn car = new CarEn("丰田",11);
+		car.setName("大众");
+		car.setPrice(-20.5);		
+		car.getCarInfo();
+	}
+}
